7632e66a4f28238d3daab415c2f9ba44944efb54
Vicente Calderón <thunderwearsvicho37@protonmail.com>
2021-09-28 23:42:16 -0300

    Menu init

--- proyetoDistribuidos/src/com/company/Main.java
+++ proyetoDistribuidos/src/com/company/Main.java
@@ -3,23 +3,76 @@ package com.company;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 
 public class Main {
 
-    int[][] matriz_pixeles;
+	int[][] matriz_pixeles;
 
-    public static void main(String[] args) throws IOException {
-        
-        try {
-            LeerImagen leer = new LeerImagen();
-            leer.matrizImagen("C:\\Users\\felip\\Documents\\GitHub\\pruebas-de-codigo-y-algoritmos\\proyetoDistribuidos\\src\\com\\company\\pruebafinalfinal.pgm");
-            //ImageIO.write(img,"png",new File("C:/lawea.png"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		try {
+			boolean salir = false;
+			Menu menu = new Menu();
+			Scanner scanner = new Scanner(System.in);
+			menu.entrada();
+			do {
+				menu.opciones();
+				System.out.print("Opción: ");
+				int opcion = scanner.nextInt();
+				System.out.println("\n");
+				switch (opcion) {
+					case 1:
+						System.out.println("Has seleccionado la opcion dilatación secuencial");
+						break;
+					case 2:
+						System.out.println("Has seleccionado la opcion dilatación paralela");
+						break;
+					case 3:
+						System.out.println("Has seleccionado la opcion erosión secuencial");
+						break;
+					case 4:
+						System.out.println("Has seleccionado la opcion erosión paralela");
+						break;
+					case 5:
+						salir = true;
+						break;
+					default:
+						System.out.println("Solo números entre 1 y 5");
+				}
+				int filtro = scanner.nextInt();
+				switch (filtro) {
+					case 1:
+						System.out.println("Has seleccionado la opcion dilatación secuencial");
+						break;
+					case 2:
+						System.out.println("Has seleccionado la opcion dilatación paralela");
+						break;
+					case 3:
+						System.out.println("Has seleccionado la opcion erosión secuencial");
+						break;
+					case 4:
+						System.out.println("Has seleccionado la opcion erosión paralela");
+						break;
+					case 5:
+						System.out.println("Has seleccionado la opcion erosión paralela");
+						break;
+					case 6:
+						salir = true;
+						break;
+					default:
+						System.out.println("Solo números entre 1 y 5");
+				}
+			} while (!salir);
+			LeerImagen leer = new LeerImagen();
+			leer.matrizImagen(
+					"C:\\Users\\felip\\Documents\\GitHub\\pruebas-de-codigo-y-algoritmos\\proyetoDistribuidos\\src\\com\\company\\pruebafinalfinal.pgm");
+			// ImageIO.write(img,"png",new File("C:/lawea.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- proyetoDistribuidos/src/com/company/Menu.java
+++ proyetoDistribuidos/src/com/company/Menu.java
@@ -2,26 +2,36 @@ package com.company;
 
 public class Menu {
 
-    public Menu(){}
+	public Menu() {
+	}
 
-    public void entrada(){
-        System.out.println("*PRO***************************************************");
-        System.out.println("****YEC************************************************");
-        System.out.println("*******TO**********************************************");
-        System.out.println("*************Distribuidos******************************");
-        System.out.println("*******************************************************");
-        System.out.println("*Matias Erenchun***************************************");
-        System.out.println("******************Vicente Calderon*********************");
-        System.out.println("************************************Felipe Olave*******");
+	public void entrada() {
+		System.out.println("*PRO***************************************************");
+		System.out.println("****YEC************************************************");
+		System.out.println("*******TO**********************************************");
+		System.out.println("*************Distribuidos******************************");
+		System.out.println("*******************************************************");
+		System.out.println("*Matias Erenchun***************************************");
+		System.out.println("******************Vicente Calderon*********************");
+		System.out.println("************************************Felipe Olave*******");
 
-    }
+	}
 
-    public void opciones(){
-        System.out.println("1) Dilatación Secuencial");
-        System.out.println("2) Dilatación Paralela");
-        System.out.println("3) Erosión Secuencial");
-        System.out.println("4) Erosión Paralela");
-        System.out.println("5) Salir");
-    }
+	public void opciones() {
+		System.out.println("1) Dilatación Secuencial");
+		System.out.println("2) Dilatación Paralela");
+		System.out.println("3) Erosión Secuencial");
+		System.out.println("4) Erosión Paralela");
+		System.out.println("5) Salir");
+	}
+
+	public void subOpciones() {
+		System.out.println("1) Filtro X");
+		System.out.println("2) Filtro +");
+		System.out.println("3) Filtro pilar");
+		System.out.println("4) Filtro horizontal");
+		System.out.println("5) Filtro esquina inferior");
+		System.out.println("6) Filtro esquina superior");
+	}
 
 }
