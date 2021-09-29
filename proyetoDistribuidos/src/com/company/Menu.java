84b0d7546d5aaf737109820023f6f982a808dfcd
Felipe <felipe.olave.r@hotmail.com>
2021-09-28 23:15:09 -0300

    Menu
    menu de opciones

---
+++ proyetoDistribuidos/src/com/company/Menu.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Menu {
+
+    public Menu(){}
+
+    public void entrada(){
+        System.out.println("*PRO***************************************************");
+        System.out.println("****YEC************************************************");
+        System.out.println("*******TO**********************************************");
+        System.out.println("*************Distribuidos******************************");
+        System.out.println("*******************************************************");
+        System.out.println("*Matias Erenchun***************************************");
+        System.out.println("******************Vicente Calderon*********************");
+        System.out.println("************************************Felipe Olave*******");
+
+    }
+
+    public void opciones(){
+        System.out.println("1) Dilatación Secuencial");
+        System.out.println("2) Dilatación Paralela");
+        System.out.println("3) Erosión Secuencial");
+        System.out.println("4) Erosión Paralela");
+        System.out.println("5) Salir");
+    }
+
+}
