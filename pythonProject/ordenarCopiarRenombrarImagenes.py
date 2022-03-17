e437739ec187c0f5db282091adfc1033390c94c4
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-03-16 22:02:40 -0300

    bueno ya esta la base de la herramienta

--- pythonProject/ordenarCopiarRenombrarImagenes.py
+++ pythonProject/ordenarCopiarRenombrarImagenes.py
@@ -1,16 +1,35 @@
 import os
+import cv2
+
+
+def crear_carpeta(b):
+    success = False
+    try:
+        os.mkdir(b)
+        success = True
+    except FileExistsError:
+        print("el directorio ya existe")
+    except FileNotFoundError:
+        print("error en la ruta")
+    return success
+
 
 directorioBase = "D:\\fotos pajaros\\imagenes"
 
-nombreDir=[]
+nombreDir = []
 with os.scandir(directorioBase) as carpetas:
     for carpeta in carpetas:
         nombreDir.append(carpeta.name)
 print(nombreDir)
-for dirActual in nombreDir:
-    misDir=[]
-    print(directorioBase+'\\'+dirActual)
-    misDir = os.listdir(directorioBase+'\\'+dirActual)
-    print(misDir)
-
-
+if crear_carpeta("D:\\fotos pajaros\\train"):
+    for dirActual in nombreDir:
+        print(directorioBase + '\\' + dirActual)
+        misDir = os.listdir(directorioBase + '\\' + dirActual)
+        print(misDir)
+        i = 0
+        for imagenActul in misDir:
+            imagen = cv2.imread(directorioBase + '\\' + dirActual + '\\' + imagenActul, -1)
+            cv2.imshow('imagen', imagen)
+            cv2.imwrite("D:\\fotos pajaros\\train\\%s%i.jpg" % (dirActual, i), imagen)
+            i += 1
+            cv2.destroyWindow('imagen')
