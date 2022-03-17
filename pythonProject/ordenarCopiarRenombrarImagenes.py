7cdf9406b16555c6761d7e26938e68ef09993b63
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-03-17 19:43:06 -0300

    terminamos la separación de las imágenes

--- pythonProject/ordenarCopiarRenombrarImagenes.py
+++ pythonProject/ordenarCopiarRenombrarImagenes.py
@@ -1,7 +1,12 @@
 import os
+from time import sleep
+
 import cv2
 
 
+# la funcion recibe un string que representa la ruta y carpeta crear
+# al ejecutar la funccion retorna la variable sucess con una valor True si se creo la carpeta
+# en caso de que ocurriera algun error retornra success con un valor False.
 def crear_carpeta(b):
     success = False
     try:
@@ -14,7 +19,7 @@ def crear_carpeta(b):
     return success
 
 
-directorioBase = "D:\\fotos pajaros\\imagenes"
+directorioBase = "D:\\DataSets\\imagenes"
 
 nombreDir = []
 with os.scandir(directorioBase) as carpetas:
@@ -24,12 +29,29 @@ print(nombreDir)
 if crear_carpeta("D:\\fotos pajaros\\train"):
     for dirActual in nombreDir:
         print(directorioBase + '\\' + dirActual)
-        misDir = os.listdir(directorioBase + '\\' + dirActual)
-        print(misDir)
-        i = 0
-        for imagenActul in misDir:
-            imagen = cv2.imread(directorioBase + '\\' + dirActual + '\\' + imagenActul, -1)
-            cv2.imshow('imagen', imagen)
-            cv2.imwrite("D:\\fotos pajaros\\train\\%s%i.jpg" % (dirActual, i), imagen)
-            i += 1
-            cv2.destroyWindow('imagen')
+        if crear_carpeta("D:\\fotos pajaros\\train\\" + dirActual):
+            misDir = os.listdir(directorioBase + '\\' + dirActual)
+            print(misDir)
+            for dirImageActual in misDir:
+                misImagenes = os.listdir(directorioBase + '\\' + dirActual + '\\' + dirImageActual)
+                print(directorioBase + '\\' + dirActual + '\\' + dirImageActual)
+                print(misImagenes)
+                i = 0
+                for imagenActul in misImagenes:
+                    print(directorioBase + '\\' + dirActual + '\\' + dirImageActual + '\\' + imagenActul)
+                    imagen = cv2.imread(directorioBase + '\\' + dirActual + '\\' + dirImageActual + '\\' + imagenActul,
+                                        -1)
+                    '''
+                     cv2.imshow("mi imageen ", imagen)
+                    key = cv2.waitKey(20)
+                    if key == 27:  # exit on ESC
+                        break
+                    '''
+                    ruta = "D:\\fotos pajaros\\train\\" + dirActual + "\\" + dirImageActual + str(i) + ".jpg"
+                    cv2.imwrite(ruta, imagen)
+                    i += 1
+else:
+    ra = os.listdir("D:\\fotos pajaros\\train\\rapaz")
+    nora = os.listdir("D:\\fotos pajaros\\train\\no rapaz")
+    print("total rapaces:"+str(len(ra)))
+    print("total NO rapaces:" + str(len(nora)))
